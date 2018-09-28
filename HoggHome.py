19ad9abf75cac863d7117c642216d4e41044ead1
Insidious00 <43679997+Insidious00@users.noreply.github.com>
2018-09-28 22:40:28 +0100

    Update HoggHome.py

--- HoggHome.py
+++ HoggHome.py
@@ -1,8 +1,12 @@
+#importing External Libraries
+
 import speech_recognition as sr
 from gtts import gTTS
 import os
 import urllib.request
 import json
+
+#Defining the functions for conversion
 
 class CurrencyConverter:
 
@@ -25,11 +29,13 @@ class CurrencyConverter:
 
 converter = CurrencyConverter("http://data.fixer.io/api/latest?access_key=4202c616ed8a0df8ee176544488d5560")
 
+#Voice Recognition
+
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source,timeout=3,phrase_time_limit=15)
-    
+#voice transcription
 try:
     print("Transcription: " + r.recognize_google(audio))
 except sr.UnknownValueError:
@@ -37,16 +43,23 @@ except sr.UnknownValueError:
 except sr.RequestError as e:
     print("cannot obtain results; [0]", format(e))
 
+#Seperating data from string of (audio)
+
 vAudio = r.recognize_google(audio)
 vList = vAudio.split(" ")
+
+#setting Data from (Audio) to variables
 
 Var1 = int(vList[3])
 Var2 = str(vList[4].upper())
 Var3 = str(vList[6].upper())
 
+#Converting variables in functions
 
 vFinal = (converter.convert(Var1, Var2, Var3))
 print(vFinal)
+
+#Creating/ executing MP3 File
 
 tts = gTTS(text=str(vFinal), lang='en')
 tts.save("pcvoice.mp3")
