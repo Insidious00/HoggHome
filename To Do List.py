2750a3d96a411706e9c29561411d083f4608f26d
Insidious00 <43679997+Insidious00@users.noreply.github.com>
2018-09-28 22:45:45 +0100

    Add files via upload

---
+++ To Do List.py
@@ -0,0 +1,15 @@
+##To do list##
+//# - round answer to 2 d.p, joel has code
+//# - make word equivilents of popular currencies
+//# - make function to list all currencies (make in pages as is long)
+//# - make scipt to acivate main on keyword
+//# - add an error message for words that are recognised
+//#     by speach converter but not currency
+//# - add while loop to let user resay something if times out
+//# - scrape word equivilents for currencies
+//# - 
+//#
+//#
+//#
+//#
+//#
