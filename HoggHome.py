ceb7a2bdb335f120c1bb121cb5251dc6fa7d4f57
Insidious00 <43679997+Insidious00@users.noreply.github.com>
2018-09-28 19:29:42 +0100

    Add files via upload

---
+++ HoggHome.py
@@ -0,0 +1,34 @@
+import urllib.request
+import json
+
+class CurrencyConverter:
+
+    rates = []
+    
+    def __init__(self,url):
+        req = urllib.request.Request(url)
+        data = urllib.request.urlopen(req).read()
+        data = json.loads(data.decode('utf-8'))
+        self.rates = data["rates"]
+
+    def convert(self,amount, from_currency, to_currency):
+        initial_amount = amount
+        if from_currency != "EUR":
+            amount = amount / self.rates[from_currency]
+        if to_currency == "EUR":
+            return initial_amount, from_currency, '=', amount, to_currency
+        else:
+            return initial_amount, from_currency, '=', amount * self.rates[to_currency], to_currency
+
+converter = CurrencyConverter("http://data.fixer.io/api/latest?access_key=4202c616ed8a0df8ee176544488d5560")
+
+
+vAmount = int(input("Enter an amount"))
+vFromCurrency = input("Enter the currency of this amount")
+vToCurrency = input("Enter a currency to change this to")
+
+print(converter.convert(vAmount, vFromCurrency, vToCurrency))
+
+
+
+                            
