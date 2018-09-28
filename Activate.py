d721b161cfefd93112f624d6436e7db9864c8f6b
Insidious00 <43679997+Insidious00@users.noreply.github.com>
2018-09-28 23:37:59 +0100

    Update Activate.py

--- Activate.py
+++ Activate.py
@@ -1,28 +1,6 @@
 import speech_recognition as sr
 import os
 
-#for i in range(0,9999999999999):
-#    r = sr.Recognizer()
-#    with sr.Microphone() as source:
-#        print("Say something!")
-#        audio = r.listen(source,timeout=3,phrase_time_limit=3)
-#    try:
-#        print("Transcription: " + r.recognize_google(audio))
-#        vAudio = r.recognize_google(audio)
-#    except sr.UnknownValueError:
-#        print("Audio is unintelligable")
-#    except sr.RequestError as e:
-#        print("cannot obtain results; [0]", format(e))
-#
-#    if vAudio.upper() == "OKAY HOMEWORK" or "OK HOMEWORK":
-#        print(vAudio)
-#        filepath = 'textfile.txt'
-#        os.startfile("F:\Python\HoggHome.py")
-#        valid = True
-#        break
-#    break
-
-
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say Something")
